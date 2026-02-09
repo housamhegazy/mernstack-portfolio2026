3712b47a0b12d8e55353ae7f7301de436c646da3
housamhegazy <geohousam@gmail.com>
2026-02-09 23:43:54 +0300

    first commit

---
+++ server/index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const bodyParser = require("body-parser"); // لاستخدامها لقراءة JSON من الطلبات
+const cors = require("cors"); // للسماح لـ frontend بالاتصال بـ backend
+const cookieParser = require("cookie-parser");
+require("dotenv").config();
+const mongoose = require("mongoose");
+const app = express();
+const port = process.env.PORT || 3000;
+
+// ********************** Middleware **********************
+app.use(cookieParser()); // خاصه بقراءة الكوكيز من الطلبات ولازم تتواجد قبل اي روت
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  }),
+); // تفعيل CORS للسماح لـ frontend (الذي يعمل على منفذ مختلف) بالاتصال بـ backend
+app.use(express.json()); // عشان السيرفر يفهم الـ JSON اللي جاي من الفرونت
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+const mongoURI = process.env.MONGODB_URI;
+
+mongoose
+  .connect(mongoURI)
+  .then(() => {
+    console.log("✅ Connected to MongoDB!");
+    // مش هنشغل السيرفر إلا لما الداتابيز تشبك
+    app.listen(port, () => {
+      console.log(`🚀 Server is running on port ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.error("❌ Could not connect to MongoDB...", err);
+  });
